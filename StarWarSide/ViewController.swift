499634cf7fbd28a9807f2e896d43b5d84143ce61
supanut1911 <ynotmela@gmail.com>
2020-04-29 16:32:15 +0700

    Initial Commit

---
+++ StarWarSide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWarSide
+//
+//  Created by Supanut Laddayam on 29/4/2563 BE.
+//  Copyright © 2563 Supanut LDM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
